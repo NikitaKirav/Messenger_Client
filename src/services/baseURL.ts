66196aaa0be3d44a7673cfc40712551c30c49dc9
hazard3d66 hazard3d66 <nikita@kirav.ru>
2022-07-07 09:20:02 +0300

    Updated http protocols

--- src/services/baseURL.ts
+++ src/services/baseURL.ts
@@ -1,11 +1,11 @@
 export const baseUrl_Http = process.env.NODE_ENV === 'production' 
-    ? window.location.protocol + '//' + window.location.hostname + ':80/api-messanger/' 
+    ? window.location.protocol + '//' + window.location.hostname + '/api-messanger/' 
     : window.location.protocol + '//' + window.location.hostname + ':4040/api-messanger/';
 
 export const baseUrl_Ws = process.env.NODE_ENV === 'production' 
-    ? 'ws://' + window.location.hostname + ':80/wss/' 
+    ? 'ws://' + window.location.hostname + '/wss/' 
     : 'ws://' + window.location.hostname + ':8081';
 
 export const baseUrl = process.env.NODE_ENV === 'production' 
-    ? window.location.protocol + '//' + window.location.hostname + ':80' 
+    ? window.location.protocol + '//' + window.location.hostname 
     : window.location.protocol + '//' + window.location.hostname + ':4040';
