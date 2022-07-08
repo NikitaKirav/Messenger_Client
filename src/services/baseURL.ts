40c278acd2fb2878057c052aaea5ce869e0f8b16
hazard3d66 hazard3d66 <nikita@kirav.ru>
2022-07-08 12:37:25 +0300

    Updated base urls

--- src/services/baseURL.ts
+++ src/services/baseURL.ts
@@ -3,7 +3,7 @@ export const baseUrl_Http = process.env.NODE_ENV === 'production'
     : window.location.protocol + '//' + window.location.hostname + ':4040/api-messanger/';
 
 export const baseUrl_Ws = process.env.NODE_ENV === 'production' 
-    ? 'ws://' + window.location.hostname + '/wss/' 
+    ? 'wss://' + window.location.hostname + '/wss/' 
     : 'ws://' + window.location.hostname + ':8081';
 
 export const baseUrl = process.env.NODE_ENV === 'production' 
